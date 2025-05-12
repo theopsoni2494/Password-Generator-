604c81e39e01f33ce54788b7779002f40774035a
Yash Soni <95700840+theopsoni2494@users.noreply.github.com>
2025-05-12 12:46:47 +0530

    Add files via upload
    I have written two codes the first one is commented and i have used Tkinter because with Tkinter, provides a quick and easy way to build GUI applications.

---
+++ pass_gen.py
@@ -0,0 +1,29 @@
+# import random
+#characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&amp;*1234567890"
+#length = int(input("Enter Password length :- "))
+#password = ""
+#for i in range(length+1):
+ #   password += random.choice(characters)
+#print(password) 
+
+import tkinter as tk
+import string
+import random
+def generate_password():
+    password = []
+    for i in range(5):
+        alpha = random.choice(string.ascii_letters)
+        symbol = random.choice(string.punctuation)
+        numbers = random.choice(string.digits)
+        password.append(alpha)
+        password.append(symbol)
+        password.append(numbers)
+        passwords = " ".join(str(x)for x in password)
+        label.config(text=passwords)
+root = tk.Tk()
+root.geometry("400x300")
+button = tk.Button(root, text="Generate Password", command=generate_password)
+button.grid(row=1, column=1)
+label = tk.Label(root, font=("times", 15, "bold"))
+label.grid(row=4, column=2)
+root.mainloop()
